43c95eb26eb6e48218dcc4d7b880af3dd753969f
Zoya <byzyka1986@mail.ru>
2024-02-08 14:32:47 +0300

    map

---
+++ src/js/__tests__/testError.js
@@ -0,0 +1,11 @@
+import ErrorRepository from "../app";
+
+test(`getExistsError`, ()=>{
+    const errorRepository = new ErrorRepository();
+    expect(errorRepository.translate(1)).toBe(`Error1`);
+})
+
+test(`getNoExistsError`, ()=>{
+    const errorRepository = new ErrorRepository();
+    expect(()=>errorRepository.translate(3)).toThrow()
+})
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,17 @@
-// TODO: write your code here
-import sum from './basic';
 
-console.log('worked');
+export default class ErrorRepository {
+    constructor(){
+        this.errors = new Map([
+            [1, 'Error1']
+        ])
+    }
+    translate(code){
+        if(!this.errors.has(code)){
+            throw new Error('Unknow error');
+        }else {
+            return this.errors.get(code);
+        }
+    }
+}
 
-console.log(sum([1, 2]));
+
